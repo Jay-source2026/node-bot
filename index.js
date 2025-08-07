c7d8140f00c8811401fc9acd9634b6f59e5a0247
Jay-source2026 <nordjerd@outlook.pt>
2025-08-07 12:30:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -231,7 +231,7 @@ bot.on('callback_query', (callbackQuery) => {
       reply +=
 `💳 *PayPal Payment*
 
-Send to: \`https://www.paypal.com/ncp/payment/FHB2D9HYLWMNU\`
+Send to: [Pay Here] (\`https://www.paypal.com/ncp/payment/FHB2D9HYLWMNU\`)
 
 Then type *confirm* once done.`;
     } else if (method === 'binance') {
@@ -264,6 +264,7 @@ Then type *confirm* once done.`;
 
   bot.answerCallbackQuery(callbackQuery.id);
 });
+
 
 
 
